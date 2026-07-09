90dc028e3a020f23da298da9c077df13c3676396
Aman SIngh <136971175+Amansinghhggg@users.noreply.github.com>
2026-07-09 20:34:00 +0530

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let max = 0
+        for(let i =0;i<prices.length-1;i++){
+            let prof= 0
+            for(let j=i+1;j<prices.length;j++){
+                prof = prices[j]-prices[i]
+                max=Math.max(max,prof)
+            }
+        }
+        return max
+    }
+}
+
