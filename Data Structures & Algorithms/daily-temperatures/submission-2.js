5cc6b4e42da9a8edc2ce03ff58bbfdf88613e5c9
Aman Singh <136971175+Amansinghhggg@users.noreply.github.com>
2026-08-13 13:20:50 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+       let stack = []
+       let res = []
+       for(let i=temperatures.length-1;i>=0;i--){
+            while(stack.length>0&&temperatures[i]>=temperatures[stack[stack.length-1]]){
+                stack.pop()
+            }
+            if(stack.length>0){
+                res[i] =  stack[stack.length - 1] - i;
+            }else{
+                res[i]= 0
+            }
+            stack.push(i)
+
+        }
+        return res
+    }
+}
