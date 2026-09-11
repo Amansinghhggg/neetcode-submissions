079b89c519cd2c0bae76d51dfe2da4580e04507d
Aman Singh <136971175+Amansinghhggg@users.noreply.github.com>
2026-09-11 16:27:04 +0530

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        
+        let map = new Map()
+        for(let i = 0 ;i<strs.length;i++){
+            let sort = strs[i].split("").sort().join("")
+            if(!map.has(sort)) map.set(sort,[])
+            map.get(sort).push(strs[i])
+        }
+        return Array.from(map.values())
+    }
+}
